925805057a4a717672d0e20d4f9f73d3d984b41e
shuvxm <kumarshubham5559@gmail.com>
2024-12-20 19:19:58 +0530

    working on connecting frontend with backend

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -5,3 +5,8 @@ export const createRoomApi = async(roomDetails) => {
     const response = await httpClient.post(`/api/v1/rooms`, roomDetails);
     return response.data;
 };
+
+export const joinChatApi = async (roomId) => {
+    const response = await httpClient.get(`/api/v1/rooms/${roomId}`);
+    return response.data;
+};
