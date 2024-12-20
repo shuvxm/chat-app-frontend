21c5bc871ebd76a390c9b6efbe4fdd19b0f38628
shuvxm <kumarshubham5559@gmail.com>
2024-12-20 19:21:18 +0530

    working on connecting frontend with backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import { BrowserRouter} from 'react-router'
-import AppRoutes from './config/routes'
-import { Toaster } from 'react-hot-toast'
+import { BrowserRouter } from "react-router";
+import AppRoutes from "./config/routes";
+import { Toaster } from "react-hot-toast";
+import { ChatProvider } from "./context/ChatContext.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <Toaster position="top-right" />
-      <AppRoutes />
+      <Toaster position="top-right" />
+      <ChatProvider>
+        <AppRoutes />
+      </ChatProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
